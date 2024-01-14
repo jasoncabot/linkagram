6eed2cef5b16fb12df806663215b46c255395d46
Jason Cabot <jasoncabot@gmail.com>
2024-01-14 17:40:47 +0000

    Onboard to analytics engine for time series data

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -9,6 +9,7 @@ export interface Env {
   PAYMENT_SERVICE: Fetcher;
   PAYMENT_SERVICE_URL: string;
   ASSETS: any;
+  ANALYTICS: AnalyticsEngineDataset;
 }
 
 export async function onRequest(context: {
@@ -22,26 +23,52 @@ export async function onRequest(context: {
   // Serve up some dynamic text and data
   if (pathname === "/assets/sample.png") {
     const { letters } = boardAndSolutionsForToday();
-    return context.env.IMAGE_GENERATOR.fetch(request, {
-      method: 'POST',
-      body: JSON.stringify(letters)
+    return env.IMAGE_GENERATOR.fetch(request, {
+      method: "POST",
+      body: JSON.stringify(letters),
     });
   } else if (pathname === "/hint_payment") {
-    return fetch(env.PAYMENT_SERVICE_URL, {method: request.method});
+    return fetch(env.PAYMENT_SERVICE_URL, { method: request.method });
+  } else if (pathname === "/stats" && request.method === "POST") {
+
+    // read the hints used and time taken from the request body
+    const body = await request.json() as {
+      hintsUsed: number;
+      timeTaken: number;
+      streak: number;
+      maxStreak: number;
+    };
+    const hintsUsed = body.hintsUsed;
+    const timeTaken = body.timeTaken;
+    const streak = body.streak;
+    const maxStreak = body.maxStreak;
+
+    const dataPoint: AnalyticsEngineDataPoint = {
+      blobs: [keyForToday()],
+      doubles: [hintsUsed, timeTaken, streak, maxStreak],
+      indexes: [request.cf.postalCode],
+    };
+
+    console.log("dataPoint", dataPoint);
+    if (env.ANALYTICS) {
+      env.ANALYTICS.writeDataPoint(dataPoint);
+    }
   } else if (pathname === "/") {
     const { words, letters } = boardAndSolutionsForToday();
-    const asset = await context.env.ASSETS.fetch(context.request.url)
-    let response = new Response(asset.body, asset)
-    return new HTMLRewriter().on('meta', new MetaUpdater(words.size, letters, origin)).transform(response);
+    const asset = await env.ASSETS.fetch(context.request.url);
+    let response = new Response(asset.body, asset);
+    return new HTMLRewriter()
+      .on("meta", new MetaUpdater(words.size, letters, origin))
+      .transform(response);
   } else {
-    // if we are using images of the form: /assets/linkagramgame.png - we treat everything in 
+    // if we are using images of the form: /assets/linkagramgame.png - we treat everything in
     // the path name as the dynamic set of letters to produce - it means we can cache it forever
     const assetRouteMatch = pathname.match(/assets\/([a-z]+)\.png/);
     if (assetRouteMatch?.length == 2) {
       const letters = assetRouteMatch[1].split("");
-      return context.env.IMAGE_GENERATOR.fetch(context.request, {
-        method: 'POST',
-        body: JSON.stringify(letters)
+      return env.IMAGE_GENERATOR.fetch(context.request, {
+        method: "POST",
+        body: JSON.stringify(letters),
       });
     }
   }
@@ -59,21 +86,32 @@ class MetaUpdater {
     this.origin = origin;
   }
   element(element: HTMLElement) {
-    if (element.getAttribute('property') === 'og:description') {
-      element.setAttribute('content', `${this.count} word${this.count === 1 ? "" : "s"} to find today. Play now to find them all.`);
-    } else if (element.getAttribute('property') === 'og:image') {
-      element.setAttribute('content', `${this.origin}/assets/${this.letters.join("")}.png`);
+    if (element.getAttribute("property") === "og:description") {
+      element.setAttribute(
+        "content",
+        `${this.count} word${
+          this.count === 1 ? "" : "s"
+        } to find today. Play now to find them all.`
+      );
+    } else if (element.getAttribute("property") === "og:image") {
+      element.setAttribute(
+        "content",
+        `${this.origin}/assets/${this.letters.join("")}.png`
+      );
     }
   }
 }
 
-const boardAndSolutionsForToday: () => { words: Set<string>, letters: string[] } = () => {
+const boardAndSolutionsForToday: () => {
+  words: Set<string>;
+  letters: string[];
+} = () => {
   const id = parseInt(keyForToday(), 10);
   const config = {
     size: { width: 4, height: 4 },
     id: id,
     dictionary: "small.json",
-    frequencies: 'letters.json'
+    frequencies: "letters.json",
   };
   const setOfWords = new Set<string>(smallWords);
   const key = `${config.id},${config.dictionary},${config.frequencies},${config.size.width},${config.size.height}`;
@@ -89,12 +127,12 @@ const boardAndSolutionsForToday: () => { words: Set<string>, letters: string[] }
     played: [],
     streak: 0,
     fixes: new Set<string>(),
-    save: (_) => { },
-    purge: () => { },
+    save: (_) => {},
+    purge: () => {},
   });
   game.initialise(smallWords, letterDistribution);
   return {
     words: game.wordList.words,
-    letters: game.tiles.map(tile => tile.value)
-  }
-}
+    letters: game.tiles.map((tile) => tile.value),
+  };
+};
